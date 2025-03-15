0e4f51d6f6e77377e31b6e1a0c1d841780ee3b7b
VictorHugoS21 <victorhugo21006@live.com>
2025-03-14 22:29:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+
+var peso;
+var altura;
+var IMC;
+var resultado;
+
+
 function calcular(event){
-    alert("teste")
+    event.preventDefault();
+
+    peso = document.getElementById('peso').value;
+    altura = document.getElementById('altura').value;
+
+    IMC = peso / (altura * altura);
+
+    if(IMC < 17){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + IMC + '<br/> Cuidado você está muito abaixo do peso!'
+    }else if(IMC > 17 && IMC <= 18.49){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + IMC + '<br/> Você abaixo do peso!';
+    }else if(IMC > 18.5 && IMC <= 24.99){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + IMC + '<br/> Você está no seu peso ideal!';
+    }else if(IMC > 25 && IMC <= 29.99){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + IMC + '<br/> Você está acima do peso!';
+    }else if(IMC >= 30){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + IMC + '<br/> Cuidado, obesidade!';
+    }
+
 }
