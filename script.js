c1094b7c3988e2ff77d8465553fc1c66fbeb08e5
VictorHugoS21 <victorhugo21006@live.com>
2025-03-21 19:18:28 -0300

    FINALIZAÇÃO DO CSS

--- script.js
+++ script.js
@@ -15,19 +15,19 @@ function calcular(event){
 
     if(IMC < 17){
         resultado = document.getElementById('resultado');
-        resultado.innerHTML = '<br/> Seu resultado foi: ' + IMC + '<br/> Cuidado você está muito abaixo do peso!'
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + IMC.toFixed(2) + '<br/> Cuidado você está muito abaixo do peso!'
     }else if(IMC > 17 && IMC <= 18.49){
         resultado = document.getElementById('resultado');
-        resultado.innerHTML = '<br/> Seu resultado foi: ' + IMC + '<br/> Você abaixo do peso!';
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + IMC.toFixed(2) + '<br/> Você abaixo do peso!';
     }else if(IMC > 18.5 && IMC <= 24.99){
         resultado = document.getElementById('resultado');
-        resultado.innerHTML = '<br/> Seu resultado foi: ' + IMC + '<br/> Você está no seu peso ideal!';
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + IMC.toFixed(2) + '<br/> Você está no seu peso ideal!';
     }else if(IMC > 25 && IMC <= 29.99){
         resultado = document.getElementById('resultado');
-        resultado.innerHTML = '<br/> Seu resultado foi: ' + IMC + '<br/> Você está acima do peso!';
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + IMC.toFixed(2) + '<br/> Você está acima do peso!';
     }else if(IMC >= 30){
         resultado = document.getElementById('resultado');
-        resultado.innerHTML = '<br/> Seu resultado foi: ' + IMC + '<br/> Cuidado, obesidade!';
+        resultado.innerHTML = '<br/> Seu resultado foi: ' + IMC.toFixed(2) + '<br/> Cuidado, obesidade!';
     }
 
 }
